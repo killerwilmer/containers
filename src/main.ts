9fd68a62d362c45eaa570ab856b8ad230af07e9f
Wilmer Arteaga <killerwilmer@gmail.com>
2023-09-13 13:06:39 -0500

    Add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+	const app = await NestFactory.create(AppModule);
+
+	const options = new DocumentBuilder()
+		.setTitle('Kiki Latam API')
+		.setDescription('API for managing container')
+		.setVersion('1.0')
+		.addTag('containers')
+		.build();
+
+	const document = SwaggerModule.createDocument(app, options);
+	SwaggerModule.setup('swagger', app, document);
+
+	await app.listen(3000);
 }
 bootstrap();
